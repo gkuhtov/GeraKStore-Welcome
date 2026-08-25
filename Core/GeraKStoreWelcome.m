e2527e797c70cc820cf7b2a89d20463cd96e9d84
madebygood <gkuhtov@mail.ru>
2026-08-25 15:53:18 +0800

    Auto-start welcome screen when dylib loads

--- Core/GeraKStoreWelcome.m
+++ Core/GeraKStoreWelcome.m
@@ -3,6 +3,13 @@
 
 @implementation GeraKStoreWelcome
 
+__attribute__((constructor))
+static void GeraKStoreWelcomeLoad(void) {
+    dispatch_async(dispatch_get_main_queue(), ^{
+        [GeraKStoreWelcome start];
+    });
+}
+
 + (void)start {
     [[WelcomeManager sharedManager] startWelcomeIfNeeded];
 }
